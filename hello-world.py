d0f06c227fc84d9f7b74262d094a740c856eebe0
Pulkit Grover <pulkitgrover01@gmail.com>
2024-05-16 19:43:54 +0530

    Done with oprations

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,31 @@
-print("Hello Mohit")
+print("Hello Pulkit Grover")
 print("I am learning")
+
 print(15+67)
+
+print('      My      first     code')
+
+print('lets create a tree')
+print('')
+print('  x  ')
+print(' xxx ')
+print('xxxxx')
+print('  x')
+print('  x')
+print('  x')
+
+print('Lets do math oprations')
+print(10 + 2)
+print(10 - 2)
+print(10 * 2)
+print(10 / 2)
+print(10 ** 2)
+print(10 / 3)
+print(10 // 3)
+
+print(5 + 5 * 2)
+print((5 + 5) * 2)
+
+print(10 > 5)
+
+print((1+2+3+4+5+6+7)/7)
