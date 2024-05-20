71caf9b56568286c8709ab9f67e68a5058b80776
Pulkit Grover <pulkitgrover01@gmail.com>
2024-05-20 21:19:19 +0530

    progress

---
+++ functions/functions.py
@@ -0,0 +1,52 @@
+
+'''
+def test_function():
+    print('hello')
+    test = 5 + 4
+    print(test)
+    
+def calculater(num1, num2, num3):
+    total = num1 + num2 + num3
+    print(total)
+    
+
+calculater(5,10,15)
+calculater(1,2,3)
+
+
+
+def better_calculator(num1, num2, operation):
+    if operation == 'add':
+        result = num1 + num2
+        print(f'{num1} + {num2} = {result}')
+
+    elif operation == 'subtract':
+        result = num1 - num2
+        print(f'{num1} - {num2} = {result}' )
+
+better_calculator(25,10,'add')
+
+'''
+
+
+def good_claculator(num1, num2, operation):
+    if operation == 'divide':
+        result = num1 / num2
+        print(f'{num1} / {num2} = {result}')
+    if operation == 'multiply':
+        result = num1 * num2
+        print(f'{num1} * {num2} = {result}')
+
+
+good_claculator(50,5,'divide')
+    
+    
+
+
+
+
+
+
+
+
+
